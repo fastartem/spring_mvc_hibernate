fcc31fadbe77d88e3d5f77b562642de70434b96d
Tomikus <tomikus1@gmail.com>
2021-12-01 17:11:15 +0300

    completed

---
+++ src/main/java/web/model/Car.java
@@ -0,0 +1,25 @@
+package web.model;
+
+public class Car {
+    private final String model;
+    private final String color;
+    private final int horsePower;
+
+    public Car(String model, String color, int horsePower) {
+        this.model = model;
+        this.color = color;
+        this.horsePower = horsePower;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+}
